c9be287ad207474ea519f9a763e73fbcaf683872
Suvojit <shuvojit.pal004@gmail.com>
2026-06-02 13:01:46 +0530

    Addition of Authorization middleware

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,31 @@
 const router = require("express").Router();
 
 const { setupAdmin,login} = require("../controllers/auth.controller");
+const protect = require("../middlewares/auth.middleware")
+const authorize = require("../middlewares/authorize.middleware")
 
 router.post("/setup", setupAdmin);
 router.post("/login", login)
+router.get(
+  "/me",
+  protect,
+  (req, res) => {
+    res.status(200).json({
+      success: true,
+      user: req.user,
+    });
+  }
+);
+router.get(
+  "/admin-only",
+  protect,
+  authorize("super_admin"),
+  (req, res) => {
+    res.json({
+      success: true,
+      message:
+        "Welcome Super Admin"
+    });
+  }
+);
 module.exports = router;
--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,6 +1,8 @@
 const jwt = require("jsonwebtoken");
+const AppError = require("./AppError")
 
 const generateAccessToken = (payload) => {
+   
   return jwt.sign(
     payload,
     process.env.JWT_SECRET,
@@ -11,6 +13,22 @@ const generateAccessToken = (payload) => {
   );
 };
 
+const verifyAccessToken = (token) => {
+    
+  try {
+    return jwt.verify(
+      token,
+      process.env.JWT_SECRET
+    );
+  } catch {
+    throw new AppError(
+      "Invalid or expired token",
+      401
+    );
+  }
+};
+
 module.exports = {
   generateAccessToken,
+  verifyAccessToken
 };
